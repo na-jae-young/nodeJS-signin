76d9acfe8a8324b18232e15538de7384b0095e72
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-07-29 17:19:57 +0900

    로그인 요천하는 FETCH 생성

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -1,2 +1,20 @@
 "use strict"
 console.log('hello world')
+
+const id = document.querySelector('#id')
+const psword = document.querySelector('#sword')
+const loginBtn = document.querySelector('#loginBtn')
+
+loginBtn.addEventListener('click',login);
+
+function login() {  
+    const req ={
+        id: id.ariaValueMax,
+        psword: psword.value
+        };
+    fetch("/login",{
+        method: "POST",
+        bodu:JSON.stringify(req)
+    })
+    }
+
