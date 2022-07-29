9dc97e765f65a4fcd2eafd957f14ce5ba3855225
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-07-29 15:24:11 +0900

    no message

---
+++ app.js
@@ -0,0 +1,30 @@
+/////////////////////express
+const express = require('express');
+const app = express();
+
+app.set('views', "../views")
+app.set("view engine", "ejs");
+
+const home = require('./routes/home');
+app.use('/',home)
+
+module.exports = app;
+
+
+
+
+
+
+
+// const http = require('http');
+// const app = http.createServer((req , res)=>{
+//     res.writeHead(200,{ "Content-Type":"text/html; charset=utf-8" });
+//     if(req.url === '/'){
+//         res.end("여기는 루트");
+//     }else if(req.url === "/login"){
+//         res.end("여기는 로그인 화면 입니다.");
+//     }
+// });
+// app.listen(3001,()=>{
+//     console.log("Http 로 가동한 서버");
+// })
---
+++ bin/www.js
@@ -0,0 +1,6 @@
+"use strict"
+const app = require('../app');
+const POST = 3000
+app.listen(POST,function(){
+    console.log('Server running at http://127.0.0.1:3000');
+});
