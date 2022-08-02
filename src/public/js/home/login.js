0dbce204eca2a134adfdca745a3a2852f9fb9f82
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-08-02 13:27:59 +0900

    오픈소스 프론드엔트 입히기

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -3,7 +3,7 @@
 
 const id = document.querySelector('#id')
 const psword = document.querySelector('#psword')
-const loginBtn = document.querySelector('button')
+const loginBtn = document.querySelector('#loginBtn')
 console.log('hello world',loginBtn)
 
 loginBtn.addEventListener("click",login);
